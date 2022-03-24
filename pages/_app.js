152294a4032dc6a4090e50c25d961a3dfe463a23
Farooq dad khan <farooqdaadkhan@gmail.com>
2022-03-24 10:59:44 +0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Web3ReactProvider } from '@web3-react/core'
+import { ethers } from 'ethers'
 import '../styles/globals.css'
 
+function getLibrary(provider) {
+  return new ethers.providers.Web3Provider(provider)
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Web3ReactProvider getLibrary={getLibrary}>
+     <Component {...pageProps} />
+  </Web3ReactProvider>
+  )
 }
 
 export default MyApp
